b7cfccb0e4ac7316a446e1eb8483509f4a3a4b2b
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-11 04:21:39 -0500

    Custom Inventory Rendering

--- src/main/java/net/brokenmoon/afloydironchest/ModContainer/BTWaila/IronChestTooltip.java
+++ src/main/java/net/brokenmoon/afloydironchest/ModContainer/BTWaila/IronChestTooltip.java
@@ -1,9 +1,14 @@
 package net.brokenmoon.afloydironchest.ModContainer.BTWaila;
 
+import net.brokenmoon.afloydironchest.mixin.Modded.BTWaila.GuiBlockOverlayAccessor;
 import net.brokenmoon.afloydironchest.tileEntities.*;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.render.ItemRenderer;
+import net.minecraft.client.render.entity.ItemEntityRenderer;
 import net.minecraft.core.block.entity.TileEntity;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.player.inventory.IInventory;
+import org.lwjgl.opengl.GL11;
 import toufoumaster.btwaila.BTWaila;
 import toufoumaster.btwaila.IBTWailaCustomBlockTooltip;
 import toufoumaster.btwaila.TooltipGroup;
@@ -33,7 +38,41 @@ public class IronChestTooltip implements IBTWailaCustomBlockTooltip {
             }
         }
 
+
         guiBlockOverlay.drawStringWithShadow("Stored items: " + current + "/" + max * inventory.getInventoryStackLimit(), 0);
-        guiBlockOverlay.drawInventory(inventory, 0);
+        drawInventory(guiBlockOverlay,inventory, 0, .5f);
+    }
+    private void drawInventory(GuiBlockOverlay guiBlockOverlay,IInventory inventory, int offX, float scale) {
+        int itemX = 0;
+        int itemY = 0;
+
+        GuiBlockOverlayAccessor accessor = (GuiBlockOverlayAccessor)guiBlockOverlay;
+        ItemEntityRenderer itemRender = accessor.getItemRender();
+        Minecraft theGame = accessor.getTheGame();
+        int posX = guiBlockOverlay.getPosX();
+        int offY = guiBlockOverlay.getOffY();
+
+        float invScale = 1/scale;
+        int itemWidth = (int) (16 * scale);
+        int itemsAcross = (int) (144f/(16 * scale));
+
+        GL11.glScaled(scale, scale, scale);
+        for(int i = 0; i < inventory.getSizeInventory(); ++i) {
+            ItemStack itemStack = inventory.getStackInSlot(i);
+            if (itemStack != null) {
+                int itemXPos = (int) ((32 + posX + offX + itemX * itemWidth) * invScale);
+                int itemYos = (int) ((offY + itemY * itemWidth) * invScale);
+                itemRender.renderItemIntoGUI(theGame.fontRenderer, theGame.renderEngine, itemStack, itemXPos, itemYos, 1.0F);
+                itemRender.renderItemOverlayIntoGUI(theGame.fontRenderer, theGame.renderEngine, itemStack, itemXPos, itemYos, 1.0F);
+                GL11.glDisable(2896);
+                ++itemX;
+                if (itemX >= itemsAcross) {
+                    itemX = 0;
+                    ++itemY;
+                }
+            }
+        }
+        GL11.glScaled(invScale, invScale, invScale);
+        guiBlockOverlay.addOffY(8 * (1 + itemY));
     }
 }
--- src/main/java/net/brokenmoon/afloydironchest/mixin/Modded/BTWaila/GuiBlockOverlayAccessor.java
+++ src/main/java/net/brokenmoon/afloydironchest/mixin/Modded/BTWaila/GuiBlockOverlayAccessor.java
@@ -1,8 +1,21 @@
 package net.brokenmoon.afloydironchest.mixin.Modded.BTWaila;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.render.entity.ItemEntityRenderer;
+import net.minecraft.core.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import toufoumaster.btwaila.gui.GuiBlockOverlay;
+
+import java.util.HashMap;
 
 @Mixin(value = GuiBlockOverlay.class, remap = false)
 public interface GuiBlockOverlayAccessor {
+    @Accessor
+    Minecraft getTheGame();
+    @Accessor
+    ItemEntityRenderer getItemRender();
+    @Accessor
+    HashMap<Class, Item> getEntityIconMap();
+
 }
