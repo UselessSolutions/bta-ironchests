b0cb0538150c2e861974eb1ec984514665fd898b
LukeisStuff <lukeisawesome03@gmail.com>
2024-04-21 23:33:47 -0400

    add namespace thign hopefully works

--- src/main/java/net/brokenmoon/afloydironchest/IronChestRecipes.java
+++ src/main/java/net/brokenmoon/afloydironchest/IronChestRecipes.java
@@ -8,7 +8,10 @@ import net.minecraft.core.data.registry.recipe.RecipeNamespace;
 import net.minecraft.core.data.registry.recipe.RecipeSymbol;
 import net.minecraft.core.data.registry.recipe.entry.RecipeEntryCrafting;
 import net.minecraft.core.item.ItemStack;
+import turniplabs.halplibe.helper.RecipeBuilder;
 import turniplabs.halplibe.util.RecipeEntrypoint;
+
+import static net.brokenmoon.afloydironchest.IronChestMain.MOD_ID;
 
 public class IronChestRecipes implements RecipeEntrypoint {
     public static final RecipeNamespace IRONCHESTS = new RecipeNamespace();
@@ -22,6 +25,7 @@ public class IronChestRecipes implements RecipeEntrypoint {
 
     @Override
     public void initNamespaces() {
-
+            RecipeEntrypoint.super.initNamespaces();
+            RecipeBuilder.initNameSpace(MOD_ID);
     }
 }
